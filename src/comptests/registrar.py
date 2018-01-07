bb481bb41e12361159d1fd51cba87c069c3101fd
Andrea Censi <acensi@ethz.ch>
2018-01-07 12:31:49 +0100

    misc

--- src/comptests/registrar.py
+++ src/comptests/registrar.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict, namedtuple, OrderedDict
-from compmake import Promise
-from compmake.jobs import assert_job_exists
-
-from contracts.utils import raise_desc, indent
 import os
 import sys
 import traceback
@@ -12,12 +8,15 @@ import warnings
 from conf_tools import ConfigMaster, GlobalConfig, ObjectSpec
 from conf_tools.utils import expand_string
 from contracts import contract
+from contracts.utils import raise_desc, indent
+
+from compmake import Promise
+from compmake.jobs import assert_job_exists
 from quickapp import iterate_context_names, iterate_context_names_pair
 
 from . import logger
 from .reports import (report_results_pairs, report_results_pairs_jobs,
                       report_results_single)
-
 
 __all__ = [
     'comptest',
@@ -37,7 +36,7 @@ __all__ = [
 class ComptestsRegistrar(object):
     """ Static storage """
     regular = []  # list of dict(function=f, dynamic=dynamic))
-    # Once they are scheduled we add id(x) here, so we make sure we 
+    # Once they are scheduled we add id(x) here, so we make sure we
     # don't register something twice
     regular_scheduled = set()
 
@@ -52,27 +51,33 @@ def register_single(objspec, f, dynamic):
     ts = ComptestsRegistrar.objspec2tests[objspec.name]
     ts.append(dict(function=f, dynamic=dynamic))
 
+
 def register_pair(objspec1, objspec2, f, dynamic):
     ts = ComptestsRegistrar.objspec2pairs[objspec1.name]
     ts.append(dict(objspec2=objspec2, function=f, dynamic=dynamic))
+
 
 def register_for_some_pairs(objspec1, objspec2, f, which1, which2, dynamic):
     ts = ComptestsRegistrar.objspec2testsomepairs[objspec1.name]
     ts.append(dict(objspec2=objspec2, function=f, dynamic=dynamic,
                    which1=which1, which2=which2))
 
+
 @contract(objspec=ObjectSpec, dynamic=bool)
 def register_for_some(objspec, f, which, dynamic):
     ts = ComptestsRegistrar.objspec2testsome[objspec.name]
     ts.append(dict(function=f, which=which, dynamic=dynamic))
 
+
 def register_indep(f, dynamic, args, kwargs):
     d = dict(function=f, dynamic=dynamic, args=args, kwargs=kwargs)
     ComptestsRegistrar.regular.append(d)
 
+
 def comptest(f):
     register_indep(f, dynamic=False, args=(), kwargs={})
     return f
+
 
 def check_fails(f, *args, **kwargs):
     try:
@@ -97,17 +102,21 @@ def check_fails(f, *args, **kwargs):
                 f.write(traceback.format_exc(e))
         except ImportError:
             pass
-        
+
     else:
         msg = 'Function was supposed to fail.'
         raise_desc(Exception, msg, f=f, args=args, kwargs=kwargs)
 
+
 class Wrap():
     """ Need to assign name """
+
     def __init__(self, f):
         self.f = f
+
     def __call__(self, *args, **kwargs):
         return self.f(*args, **kwargs)
+
 
 def comptest_fails(f):
     check_fails_wrap = Wrap(check_fails)
@@ -115,6 +124,7 @@ def comptest_fails(f):
     check_fails_wrap.__module__ = f.__module__
     register_indep(check_fails_wrap, dynamic=False, args=(f,), kwargs={})
     return f
+
 
 def comptest_dynamic(f):
     register_indep(f, dynamic=True, args=(), kwargs={})
@@ -149,67 +159,93 @@ def comptests_for_all_dynamic(objspec):
         Returns a decorator for mcdp_lang_tests, which should take three parameters:
         context, id_object and object.
     """
+
     def register(f):
         register_single(objspec, f, dynamic=True)
         return f
+
     return register
 
 
 @contract(objspec=ObjectSpec)
 def comptests_for_some(objspec):
     """ Returns a decorator for a test involving one object only. """
+
     def dec(which):
+
         def register(f):
             register_for_some(objspec=objspec, f=f, which=which, dynamic=False)
             return f
+
         return register
+
     return dec
 
 
 @contract(objspec=ObjectSpec)
 def comptests_for_some_dynamic(objspec):
     """ Returns a decorator for a test involving one object only. """
+
     def dec(which):
+
         def register(f):
             register_for_some(objspec=objspec, f=f, which=which, dynamic=True)
             return f
+
         return register
+
     return dec
+
 
 @contract(objspec1=ObjectSpec, objspec2=ObjectSpec)
 def comptests_for_some_pairs(objspec1, objspec2):
     """ Returns a decorator for a test involving only a subset of objects. """
+
     def dec(which1, which2):
+
         def register(f):
             register_for_some_pairs(objspec1, objspec2, f, which1, which2, dynamic=False)
             return f
+
         return register
+
     return dec
+
 
 @contract(objspec1=ObjectSpec, objspec2=ObjectSpec)
 def comptests_for_some_pairs_dynamic(objspec1, objspec2):
     """ Returns a decorator for a test involving only a subset of objects. """
+
     def dec(which1, which2):
+
         def register(f):
             register_for_some_pairs(objspec1, objspec2, f, which1, which2, dynamic=True)
             return f
+
         return register
+
     return dec
 
 
 @contract(objspec1=ObjectSpec, objspec2=ObjectSpec)
 def comptests_for_all_pairs_dynamic(objspec1, objspec2):
+
     def register(f):
         register_pair(objspec1, objspec2, f, dynamic=True)
         return f
+
     return register
+
 
 @contract(objspec1=ObjectSpec, objspec2=ObjectSpec)
 def comptests_for_all_pairs(objspec1, objspec2):
+
     def register(f):
         register_pair(objspec1, objspec2, f, dynamic=False)
         return f
+
     return register
+
 
 @contract(cm=ConfigMaster)
 def jobs_registrar(context, cm, create_reports=False):
@@ -242,31 +278,32 @@ def jobs_registrar(context, cm, create_reports=False):
 
     jobs_registrar_simple(context)
 
+
 def jobs_registrar_simple(context, only_for_module=None):
     """ Registers the simple "comptest" """
     prefix = context._job_prefix
-    
+
+    n = 0
     for x in ComptestsRegistrar.regular:
         function = x['function']
         dynamic = x['dynamic']
         args = x['args']
         kwargs = x['kwargs']
-        
+
         if only_for_module is not None:
             this = function.__module__.split(".")[0]
             if this != only_for_module:
                 msg = ('Skipping function %s in module %s because not in module %r' %
-                       (function, function.__module__, only_for_module)) 
+                       (function, function.__module__, only_for_module))
 #                 logger.error(msg)
                 continue
-                
-        
+
         id_x = id(x)
         if id_x in ComptestsRegistrar.regular_scheduled:
             msg = 'Job already registered. Skipping.'
             logger.debug(msg)
             continue
-        
+
         ComptestsRegistrar.regular_scheduled.add(id_x)
 
         # print('registering %s' % x)
@@ -277,22 +314,29 @@ def jobs_registrar_simple(context, only_for_module=None):
         else:
             _res = context.comp_config_dynamic(wrapper, *args, **kwargs)
 
+        n += 1
+
+    print('Registered %d tests (reading a list of %s)' % (n, len(ComptestsRegistrar.regular)))
+
+
 class WrapTest(object):
+
     def __init__(self, function, prefix):
         self.__name__ = function.__name__
         self.function = function
         from .comptests import CompTests
         if prefix is not None:
-            self.output_dir = os.path.join(CompTests.global_output_dir, 
+            self.output_dir = os.path.join(CompTests.global_output_dir,
                                            prefix, self.__name__)
         else:
-            self.output_dir = os.path.join(CompTests.global_output_dir, 
+            self.output_dir = os.path.join(CompTests.global_output_dir,
                                            self.__name__)
-            
+
     def __call__(self, *args, **kwargs):
         from .comptests import CompTests
         CompTests.output_dir_for_current_test = self.output_dir
         return self.function(*args, **kwargs)
+
 
 @contract(cm=ConfigMaster,
           returns='dict(str:dict(str:str))')
@@ -318,7 +362,7 @@ def define_tests_for(context, cm, name, names2test_objects,
     define_tests_single(context, objspec, names2test_objects,
                         functions=functions, create_reports=create_reports)
     define_tests_pairs(context, objspec, names2test_objects,
-                       pairs=pairs,create_reports=create_reports)
+                       pairs=pairs, create_reports=create_reports)
 
     define_tests_some_pairs(context, objspec, names2test_objects,
                             some_pairs=some_pairs, create_reports=create_reports)
@@ -464,7 +508,7 @@ def define_tests_pairs(context, objspec1, names2test_objects, pairs, create_repo
             ob1 = Promise(objs1[id_ob1])
             ob2 = Promise(objs2[id_ob2])
 
-            params=dict(job_id='f', command_name=func.__name__)
+            params = dict(job_id='f', command_name=func.__name__)
             if dynamic:
                 res = c.comp_config_dynamic(wrap_func_pair_dyn,
                                             func, id_ob1, ob1, id_ob2, ob2,
@@ -536,6 +580,7 @@ def define_tests_some_pairs(context, objspec1, names2test_objects, some_pairs, c
         use_objs2 = dict((k, allobjs2[k]) for k in objs2)
         define_tests_some_pairs_(cx, db, objspec1, objspec2, use_objs1, use_objs2, func, dynamic, create_reports)
 
+
 def define_tests_some_pairs_(cx, db, objspec1, objspec2, objs1, objs2, func, dynamic, create_reports):
     results = {}
     jobs = {}
@@ -559,7 +604,6 @@ def define_tests_some_pairs_(cx, db, objspec1, objspec2, objs1, objs2, func, dyn
         results[(id_ob1, id_ob2)] = res
         jobs[(id_ob1, id_ob2)] = res.job_id
 
-
     if create_reports:
         r = cx.comp_dynamic(report_results_pairs_jobs,
                              func, objspec1.name, objspec2.name, jobs)
@@ -574,19 +618,23 @@ def wrap_func(func, id_ob1, ob1):
     # print('%20s: %s' % (id_ob1, describe_value(ob1)))
     return func(id_ob1, ob1)
 
+
 def wrap_func_dyn(context, func, id_ob1, ob1):
     # print('%20s: %s' % (id_ob1, describe_value(ob1)))
-    return func(context, id_ob1,ob1)
+    return func(context, id_ob1, ob1)
+
 
 def wrap_func_pair_dyn(context, func, id_ob1, ob1, id_ob2, ob2):
     # print('%20s: %s' % (id_ob1, describe_value(ob1)))
     # print('%20s: %s' % (id_ob2, describe_value(ob2)))
-    return func(context, id_ob1,ob1,id_ob2,ob2)
+    return func(context, id_ob1, ob1, id_ob2, ob2)
+
 
 def wrap_func_pair(func, id_ob1, ob1, id_ob2, ob2):
     # print('%20s: %s' % (id_ob1, describe_value(ob1)))
     # print('%20s: %s' % (id_ob2, describe_value(ob2)))
-    return func(id_ob1,ob1,id_ob2,ob2)
+    return func(id_ob1, ob1, id_ob2, ob2)
+
 
 @contract(objspec=ObjectSpec, returns='dict(str:str)')
 def get_testobjects_promises_for_objspec(context, objspec):
@@ -604,7 +652,7 @@ def get_testobjects_promises_for_objspec(context, objspec):
     promises = {}
     for id_object in objects:
         params = dict(job_id='%s-instance-%s' % (objspec.name, id_object),
-                      command_name='instance_%s' %objspec.name)
+                      command_name='instance_%s' % objspec.name)
         if objspec.instance_method is None:
             job = context.comp_config(get_spec, master_name=objspec.master.name,
                                   objspec_name=objspec.name, id_object=id_object,
@@ -644,6 +692,7 @@ def get_objspec(master_name, objspec_name):
     objspec = master.specs[objspec_name]
     return objspec
 
+
 def run_module_tests():
     """
         Runs directly the tests defined in this module.
@@ -655,6 +704,7 @@ def run_module_tests():
     """
 #     logger.debug('run_module_tests: args = %s' % sys.argv)
     grep = sys.argv[1] if len(sys.argv) > 1 else None
+
 #     logger.debug('grep = %r' % grep)
     def should_ignore(its_name):
         if grep is None:
@@ -663,7 +713,6 @@ def run_module_tests():
 #         if not do_ignore:
 #             print('found %s in %s' % ( grep, r))
         return do_ignore
-
 
     Res = namedtuple('Res', 'x es en')
     results = OrderedDict()
@@ -683,7 +732,6 @@ def run_module_tests():
             continue
 
         logger.debug('Running test %s' % name)
-
 
         try:
             wrapped = WrapTest(function, prefix=None)
@@ -727,7 +775,6 @@ def run_module_tests():
 #             pass
 #             logger.debug('skipping %s because not in module __main__ (%s)' %
 #                           (name, function.__module__))
-
 
     if nerrors > 0 and (len(seen) > 1):
         logger.error('There were %d errors' % nerrors)
