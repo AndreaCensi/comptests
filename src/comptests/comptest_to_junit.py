bbe692d5404cfad6c2bcdf962e75bad4fe84dbbc
Andrea Censi <acensi@ethz.ch>
2018-04-04 08:37:29 -0400

    comptests with uncompressed fs

--- src/comptests/comptest_to_junit.py
+++ src/comptests/comptest_to_junit.py
@@ -15,7 +15,19 @@ def comptest_to_junit_main():
         raise UserError(msg)
 
     dirname = args[0]
+    # try compressed
     db = StorageFilesystem(dirname, compress=True)
+    jobs = list(all_jobs(db))
+    if not jobs:
+        # try uncompressed
+        db = StorageFilesystem(dirname, compress=False)
+        jobs = list(all_jobs(db))
+
+        if not jobs:
+            msg = 'Could not find any job, compressed or not.'
+            logger.error(msg)
+            sys.exit(1)
+
     s = junit_xml(db)
     sys.stdout.write(s)
 
