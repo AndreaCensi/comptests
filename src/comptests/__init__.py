35938035813b15877cb3229d4fe9dac866423489
Andrea Censi <acensi@ethz.ch>
2021-04-24 11:04:00 +0200

    Bump to version 7.1.2104241104

--- src/comptests/__init__.py
+++ src/comptests/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "7.1.2104231703"
-__date__ = "2021-04-23T17:03:26.419790"
+__version__ = "7.1.2104241104"
+__date__ = "2021-04-24T11:04:00.835006"
 
 from zuper_commons.logs import ZLogger
 
