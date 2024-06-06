dfbdd004197f1beec1ab4292b2b16365da695777
Andrea Censi <AndreaCensi@users.noreply.github.com>
2024-06-06 13:03:17 +0200

    [ci skip]

--- src/comptests/junit_compare.py
+++ src/comptests/junit_compare.py
@@ -10,20 +10,25 @@ from zuper_zapp import zapp1, ZappEnv
 
 @zapp1()
 async def junit_compare_main(ze: ZappEnv) -> ExitCode:
+    logger = ze.sti.logger
     parser = ZArgumentParser()
+    parser.add_argument("--ignore-std", default=False, action="store_true")
 
     ignores = [
         ".*_dynreports_.*",
         ".*-context$",
         ".*-mcdplib_tst_setup_spec$",
         ".*-comptests$",
+        ".*-jobs_comptests-.*_tests$",
         ".*-nosesingle$",
-        ".*-define_tests_rendering$",
+        ".*-define_tsts_rendering$",
         ".*-sources-mcdplib_tst_setup_sources$",
-        "^ct-mcdp_tests-jobs_comptests-[^-]+$",
+        ".*-define_tsts_for_repo$",
+        # "^ct-mcdp_tests-jobs_comptests-[^-]+$",
     ]
 
     parsed, rest = parser.parse_known_args(args=ze.args)  # ok
+    ignore_std = parsed.ignore_std
     jobs: dict[str, set[str]] = {}
     tc2time: dict[str, float] = {}
     tc2bn: dict[str, set[str]] = defaultdict(set)
@@ -42,10 +47,17 @@ async def junit_compare_main(ze: ZappEnv) -> ExitCode:
         for testsuite in root.findall("testsuite"):
             for testcase in testsuite.findall("testcase"):
                 name = testcase.attrib["name"]
-                t = float(testcase.attrib["time"])
 
-                if any(re.match(ignore, name) for ignore in ignores):
-                    continue
+                # ttype = testcase.attrib.get("type", 'success')
+                # if ttype == "skip":
+                #     t = 0
+                # else:
+
+                t = float(testcase.attrib.get("time", 0))
+
+                if ignore_std:
+                    if any(re.match(ignore, name) for ignore in ignores):
+                        continue
                 jobs[fn].add(name)
                 tc2bn[name].add(bn)
                 tc2time[name] = t
@@ -54,11 +66,14 @@ async def junit_compare_main(ze: ZappEnv) -> ExitCode:
     for fn in jobs:
         counter.update(jobs[fn])
 
+    ntot = sum(len(jobs[fn]) for fn in jobs)
+
     ndups = 0
     for k in sorted(counter):
         v = counter[k]
         if v > 1:
-            print(f"{v} copies of {k} in {tc2bn[k]}")
+            print(f"{v} copies of {k}")
+            # print(f"{v} copies of {k} in {tc2bn[k]}")
             ndups += 1
 
     print(f"found {ndups} duplicates")
@@ -69,6 +84,9 @@ async def junit_compare_main(ze: ZappEnv) -> ExitCode:
     print(f"Top {N} jobs by time")
     for name in names[:N]:
         print(f"{tc2time[name]:.2f} {name:<40} ")
+
+    print(f"Total number of tests: {ntot}")
+
     return ExitCode.OK
 
 
